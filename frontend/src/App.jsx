3391b76fe2c442fce7a88e0c55a59dca3a816890
Vinay <127412984+Binay0018@users.noreply.github.com>
2025-10-02 20:30:13 +0530

    Initial commit for CampusConnect project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/DashBoard";
+import { store } from "./Store/Store";
+import { Provider } from 'react-redux'
 
 function App() {
   return (
